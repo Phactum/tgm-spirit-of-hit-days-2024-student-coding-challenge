49acbca7a02ba5224b77dc77e16b5ee31271ed9e
Laurenz Novotny <laurenz.novotny@wdw-elab.de>
2024-02-01 15:39:36 +0100

    Improving task definition type placeholder

--- studentTask/spirit-of-hit-days-student-task/src/main/java/at/phactum/studenttask/YourTask.java
+++ studentTask/spirit-of-hit-days-student-task/src/main/java/at/phactum/studenttask/YourTask.java
@@ -1,22 +1,12 @@
 package at.phactum.studenttask;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.Instant;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import io.camunda.zeebe.client.api.response.ActivatedJob;
-import io.camunda.zeebe.client.api.worker.JobClient;
 import io.camunda.zeebe.spring.client.annotation.JobWorker;
 
 @Component
@@ -25,7 +15,7 @@ public class YourTask {
 
 	private final static String INPUT_NOT_FOUND = "ProcessVariable 'input' not found";
 
-	@JobWorker(type = "count_people_under_30_group_01") // TODO: change type according to your group "count_people_under_30_group_XX". XX will be provided.
+	@JobWorker(type = "your_task_definition_type") // TODO: change type according to your task definition type
 	public Map<String, Object> countPeopleUnder30(final ActivatedJob job) {
 		// TODO: Implement worker to get job and input from process variables from ZeeBe
 		String input = "";
